5d3ed2866b00a76c9fef73e485071616e445c8a3
yazid138 <ma39isy@gmail.com>
2023-11-07 21:29:56 +0700

    balik lagi pake post

--- src/modules/blob-storage/blob-storage.controller.ts
+++ src/modules/blob-storage/blob-storage.controller.ts
@@ -1,20 +1,21 @@
-import { Controller, Get, Param, Res, StreamableFile } from "@nestjs/common";
+import { Body, Controller, Post, Res, StreamableFile } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import type { Response } from 'express';
 import { appConfig } from 'src/config/config';
 import { BlobStorageService } from './blob-storage.service';
+import { GetBlobFileDto } from './dto/blob-storage.dto';
 
 @ApiTags('blob-storage')
 @Controller('blob-storage')
 export class BlobStorageController {
   constructor(private readonly blobStorageService: BlobStorageService) {}
 
-  @Get('/:filename')
+  @Post()
   async getContent(
     @Res({ passthrough: true }) res: Response,
-    @Param('filename') filename: string,
+    @Body() query: GetBlobFileDto,
   ) {
-    const file = await this.blobStorageService.getContent(filename);
+    const file = await this.blobStorageService.getContent(query.filename);
     res.set({
       'Content-Type': 'application/pdf',
       'Content-Disposition': `inline`,
